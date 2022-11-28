b0f8b82335357163ba8c4e8db36960dd3307acbb
laurakeuda <99165907+laurakeuda@users.noreply.github.com>
2022-11-28 08:49:36 +0200

    Add project files.

---
+++ esitehtävä 1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace esitehtävä_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TekstiLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(318, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kirjoita";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TekstiLB
+            // 
+            this.TekstiLB.AutoSize = true;
+            this.TekstiLB.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TekstiLB.ForeColor = System.Drawing.Color.Red;
+            this.TekstiLB.Location = new System.Drawing.Point(318, 173);
+            this.TekstiLB.Name = "TekstiLB";
+            this.TekstiLB.Size = new System.Drawing.Size(358, 50);
+            this.TekstiLB.TabIndex = 1;
+            this.TekstiLB.Text = "Teksti tulee tähän...";
+            this.TekstiLB.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1066, 450);
+            this.Controls.Add(this.TekstiLB);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Harjoitus 1 - Laura Shpakova";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label TekstiLB;
+    }
+}
